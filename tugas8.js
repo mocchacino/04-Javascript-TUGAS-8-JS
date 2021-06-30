73e690d042ca490fc39f1aad0edf0b5bff7e967b
wiladhianty <wiladhianty@gmail.com>
2021-06-30 11:02:53 +0700

    tambah tugas

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function panggilObject(){
+    var kazuha = {
+        name : "Kazuha Kadehara",
+        vision : "Anemo",
+        gender : "Male",
+        weapon : "Sword",
+        rarity : 5,
+        availabe : false,
+    }
+    console.log(kazuha)
+    kazuha.availabe = true
+    console.log(kazuha)
+}
+
+panggilObject();
